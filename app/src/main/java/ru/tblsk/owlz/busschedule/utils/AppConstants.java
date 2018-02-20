7273f7ae8f9a871eedc1fa19129ab9c5930f3b45
Airoks <daniyrsuyuchba@gmail.com>
2018-02-21 02:33:05 +0500

    +

---
+++ app/src/main/java/ru/tblsk/owlz/busschedule/utils/AppConstants.java
@@ -0,0 +1,11 @@
+package ru.tblsk.owlz.busschedule.utils;
+
+
+
+public final class AppConstants {
+    public static final String DB_NAME = "bus_schedule.db";
+
+    private AppConstants() {
+
+    }
+}
