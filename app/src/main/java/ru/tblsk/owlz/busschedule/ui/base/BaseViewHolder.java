8cdac3d80c18adf3786fad8d1039446d9a0d0c66
Airoks <daniyrsuyuchba@gmail.com>
2018-03-09 23:53:39 +0500

    9.03

---
+++ app/src/main/java/ru/tblsk/owlz/busschedule/ui/base/BaseViewHolder.java
@@ -0,0 +1,11 @@
+package ru.tblsk.owlz.busschedule.ui.base;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public abstract class BaseViewHolder extends RecyclerView.ViewHolder{
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+    }
+}
